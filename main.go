57117af7c13fbe6163feb52ffb00251a97ac03b5
Egor Nuikin <e.nuyckin@gmail.com>
2026-07-26 21:45:51 +0400

    Expand default file-type rules with more extensions

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 var DefaultRules = map[string]string{
-	"jpg": "Images",
-	"pdf": "Documents",
-	"mp3": "Music",
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
 }
 
 func main() {
